abac65aa016fdd635ea2ebcb747351d464b62165
JulienBara <bara.julien@yahoo.fr>
2017-03-09 15:16:04 +0000

    Fix error trying to get count of null list

--- EmotionAnalyticsManagerBotFW/Controllers/MessagesController.cs
+++ EmotionAnalyticsManagerBotFW/Controllers/MessagesController.cs
@@ -39,7 +39,7 @@ namespace EmotionAnalyticsManagerBotFW
                     await connector.Conversations.ReplyToActivityAsync(reply);
                 }
 
-                if (activity.Attachments.Count > 0)
+                if (activity.Attachments != null)
                 {
                     foreach (var attachement in activity.Attachments)
                     {
