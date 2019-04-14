bfcb61e7ef19041b08a73435cfee5dbb8f171802
JulienBara <bara.julien@yahoo.fr>
2019-04-14 18:35:00 +0200

    scaffold

--- EmotionAnalyticsManagerBotFWCore/Program.cs
+++ EmotionAnalyticsManagerBotFWCore/Program.cs
@@ -1,13 +1,10 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace EmotionAnalyticsManagerBotFWCore
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
@@ -18,20 +15,9 @@ namespace EmotionAnalyticsManagerBotFWCore
             WebHost.CreateDefaultBuilder(args)
                 .ConfigureLogging((hostingContext, logging) =>
                 {
-                    // Add Azure Logging
                     logging.AddAzureWebAppDiagnostics();
-
-                    // Logging Options.
-                    // There are other logging options available:
-                    // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/logging/?view=aspnetcore-2.1
-                    // logging.AddDebug();
-                    // logging.AddConsole();
                 })
-
-                // Logging Options.
-                // Consider using Application Insights for your logging and metrics needs.
-                // https://azure.microsoft.com/en-us/services/application-insights/
-                // .UseApplicationInsights()
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
--- EmotionAnalyticsManagerBotFWCore/Startup.cs
+++ EmotionAnalyticsManagerBotFWCore/Startup.cs
@@ -1,12 +1,9 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-using System;
+﻿using System;
 using System.IO;
 using System.Linq;
+using EmotionAnalyticManagerCoreStandard;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Configuration;
 using Microsoft.Bot.Connector.Authentication;
@@ -16,9 +13,6 @@ using Microsoft.Extensions.Logging;
 
 namespace EmotionAnalyticsManagerBotFWCore
 {
-    /// <summary>
-    /// The Startup class configures services and the request pipeline.
-    /// </summary>
     public class Startup
     {
         private ILoggerFactory _loggerFactory;
@@ -32,29 +26,12 @@ namespace EmotionAnalyticsManagerBotFWCore
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
-
-            Configuration = builder.Build();
         }
 
-        /// <summary>
-        /// Gets the configuration that represents a set of key/value application configuration properties.
-        /// </summary>
-        /// <value>
-        /// The <see cref="IConfiguration"/> that represents a set of key/value application configuration properties.
-        /// </value>
-        public IConfiguration Configuration { get; }
-
-        /// <summary>
-        /// This method gets called by the runtime. Use this method to add services to the container.
-        /// </summary>
-        /// <param name="services">The <see cref="IServiceCollection"/> specifies the contract for a collection of service descriptors.</param>
-        /// <seealso cref="IStatePropertyAccessor{T}"/>
-        /// <seealso cref="https://docs.microsoft.com/en-us/aspnet/web-api/overview/advanced/dependency-injection"/>
-        /// <seealso cref="https://docs.microsoft.com/en-us/azure/bot-service/bot-service-manage-channels?view=azure-bot-service-4.0"/>
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IConfiguration configuration, IServiceCollection services)
         {
-            var secretKey = Configuration.GetSection("botFileSecret")?.Value;
-            var botFilePath = Configuration.GetSection("botFilePath")?.Value;
+            var secretKey = configuration.GetSection("botFileSecret")?.Value;
+            var botFilePath = configuration.GetSection("botFilePath")?.Value;
             if (!File.Exists(botFilePath))
             {
                 throw new FileNotFoundException($"The .bot configuration file was not found. botFilePath: {botFilePath}");
@@ -92,46 +69,21 @@ namespace EmotionAnalyticsManagerBotFWCore
                 throw new InvalidOperationException($"The .bot file does not contain an endpoint with name '{environment}'.");
             }
 
-            // Memory Storage is for local bot debugging only. When the bot
-            // is restarted, everything stored in memory will be gone.
-            IStorage dataStore = new MemoryStorage();
-
-            // For production bots use the Azure Blob or
-            // Azure CosmosDB storage providers. For the Azure
-            // based storage providers, add the Microsoft.Bot.Builder.Azure
-            // Nuget package to your solution. That package is found at:
-            // https://www.nuget.org/packages/Microsoft.Bot.Builder.Azure/
-            // Un-comment the following lines to use Azure Blob Storage
-            // // Storage configuration name or ID from the .bot file.
-            // const string StorageConfigurationId = "<STORAGE-NAME-OR-ID-FROM-BOT-FILE>";
-            // var blobConfig = botConfig.FindServiceByNameOrId(StorageConfigurationId);
-            // if (!(blobConfig is BlobStorageService blobStorageConfig))
-            // {
-            //    throw new InvalidOperationException($"The .bot file does not contain an blob storage with name '{StorageConfigurationId}'.");
-            // }
-            // // Default container name.
-            // const string DefaultBotContainer = "<DEFAULT-CONTAINER>";
-            // var storageContainer = string.IsNullOrWhiteSpace(blobStorageConfig.Container) ? DefaultBotContainer : blobStorageConfig.Container;
-            // IStorage dataStore = new Microsoft.Bot.Builder.Azure.AzureBlobStorage(blobStorageConfig.ConnectionString, storageContainer);
-
-            // Create and add conversation state.
-            var conversationState = new ConversationState(dataStore);
-            services.AddSingleton(conversationState);
-
             services.AddBot<EmotionAnalyticsManagerBotFWCoreBot>(options =>
-           {
-               options.CredentialProvider = new SimpleCredentialProvider(endpointService.AppId, endpointService.AppPassword);
+            {
+                options.CredentialProvider = new SimpleCredentialProvider(endpointService.AppId, endpointService.AppPassword);
 
-                // Catches any errors that occur during a conversation turn and logs them to currently
-                // configured ILogger.
                 ILogger logger = _loggerFactory.CreateLogger<EmotionAnalyticsManagerBotFWCoreBot>();
 
-               options.OnTurnError = async (context, exception) =>
-               {
-                   logger.LogError($"Exception caught : {exception}");
-                   await context.SendActivityAsync("Sorry, it looks like something went wrong.");
-               };
-           });
+                options.OnTurnError = async (context, exception) =>
+                {
+                    logger.LogError($"Exception caught : {exception}");
+                    await context.SendActivityAsync("Sorry, it looks like something went wrong.");
+                };
+            });
+
+            services.AddScoped<EmotionPicture>(x => new EmotionPicture("azurecognitivekey"));
+            services.AddScoped<EmotionText>(x => new EmotionText("ibmuser","ibmpwd", "yandexkey"));
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
